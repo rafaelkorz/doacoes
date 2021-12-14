9962360525f98688922ef099aa2af2955df92725
Rafael Korz <rafael.korz@gmail.com>
2021-12-13 22:00:34 -0300

    Finished admin user

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 const dbConnection = require('./db')
+app.use(cors());
 app.use(express.json())
 
 app.use('/api/users/' , require('./routes/usersRoute'))
