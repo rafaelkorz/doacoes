347848dcfe302d822af3d644ea0fb2305523f43d
Rafael Korz <rafael.korz@gmail.com>
2021-12-14 20:47:32 -0300

    Finished project

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectDB(){
-  mongoose.connect('mongodb+srv://korz:korz@cluster0.2emxk.mongodb.net/doacoes_prod?retryWrites=true&w=majority' , {useUnifiedTopology: true , useNewUrlParser: true})
+  mongoose.connect(process.env.APP_URL_DB , {useUnifiedTopology: true , useNewUrlParser: true})
   const connection = mongoose.connection
   connection.on('connected' , ()=>{
       console.log('Mongo DB Connection Successfull')
