bb3f1b66429111a6e526a41b7eab74e72cecb0ea
Rafael Korz <rafael.korz@gmail.com>
2021-12-14 21:47:48 -0300

    Project done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
 import { Provider } from 'react-redux'
+import 'dotenv/config';
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
