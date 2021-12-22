e210037883c24d274246f410f3d5516aad724f21
Rafael Korz <rafael.korz@gmail.com>
2021-12-22 11:33:53 -0300

    Refresh Token

--- frontend/src/redux/actions/donationsActions.js
+++ frontend/src/redux/actions/donationsActions.js
@@ -1,4 +1,4 @@
-import api from './../../services/api';
+import {api} from './../../services/api';
 import { message } from 'antd'
 
 export const donationReg = ( reqObj ) => async dispatch => {
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,22 +1,24 @@
 import axios from 'axios';
 import jwt_decode from "jwt-decode";
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 })
 
-const RefreshToken = async () => {
-  const user = JSON.parse(localStorage.getItem('user'))
+export const apiJWT = axios.create({
+  baseURL: process.env.REACT_APP_API_URL
+})
 
+const refreshToken = async () => {
   try {
-    const res = await api.post("/refresh", { token: user.refreshToken });
-    const data = {
-      ...user,
-      accessToken: res.data.accessToken,
-      refreshToken: res.data.refreshToken,
-    };
+    const user = JSON.parse(localStorage.getItem('user'))
 
-    localStorage.setItem('user' , JSON.stringify(data))
+    const res = await api.post("/api/users/refresh", { token: user.refreshToken });
+
+    user.accessToken = res.data.accessToken;
+    user.refreshToken = res.data.refreshToken;
+
+    localStorage.setItem('user' , JSON.stringify(user))
     
     return res.data;
   } catch (err) {
@@ -24,24 +26,23 @@ const RefreshToken = async () => {
   }
 };
 
-api.interceptors.request.use(
+apiJWT.interceptors.request.use(
   async (config) => {
     const user = JSON.parse(localStorage.getItem('user'))
-    let currentDate = new Date();
-    if (config.url !== "/api/users/login") {
-      const decodedToken = jwt_decode(user.accessToken);
 
-      if (decodedToken.exp * 1000 < currentDate.getTime()) {
-        const data = await RefreshToken();
-        config.headers["authorization"] = "Bearer " + data.accessToken;
-      }
+    config.headers["authorization"] = "Bearer " + user.accessToken;
+
+    let currentDate = new Date();
+    const decodedToken = jwt_decode(user.accessToken);
+    
+    if (decodedToken.exp * 1000 < currentDate.getTime()) {
+      const data = await refreshToken();
+      config.headers["authorization"] = "Bearer " + data.accessToken;
     }
+
     return config;
   },
   (error) => {
     return Promise.reject(error);
   }
 );
-  
-
-export default api;
