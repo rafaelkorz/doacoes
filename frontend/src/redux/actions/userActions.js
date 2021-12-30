7b289341d313965b5187a8cdffd856c15a00e919
Rafael Korz <rafael.korz@gmail.com>
2021-12-29 23:30:25 -0300

    Finished validation email and bcrypt

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -7,11 +7,14 @@ export const userLogin = ( reqObj ) => async dispatch => {
   try {
     const response = await api.post('/api/users/login', reqObj)
 
-    localStorage.setItem('user' , JSON.stringify(response.data))
-    dispatch({ type: 'GET_USER_LOGGED', payload: response.data})
-    
-    message.success('Bem vindo!')
-    
+    if (response.data.state === 1) {
+      message.warning(response.data.message)      
+    } else {
+      message.success('Bem vindo!')
+      localStorage.setItem('user' , JSON.stringify(response.data))
+      dispatch({ type: 'GET_USER_LOGGED', payload: response.data})  
+    }
+
     setTimeout(() => {
       window.location.href='/'
       dispatch({ type: 'LOADING' , payload: false})
@@ -26,22 +29,19 @@ export const userLogin = ( reqObj ) => async dispatch => {
 
 export const userRegister = ( reqObj ) => async dispatch => {
   dispatch({ type: 'LOADING' , payload: true})
-
-  try {
-    const response = await api.post('/api/users/register', reqObj)
-    
-    if (response) {
-        message.success('Usuário registrado!')
-    }
+ 
+  const response = await api.post('/api/users/register', reqObj)
+  
+  if (response.data.state === 2) {
+    message.success(response.data.message)
 
     setTimeout(() => {
-        window.location.href='/login'
-        dispatch({ type: 'LOADING' , payload: false })
+      window.location.href='/login'
+      dispatch({ type: 'LOADING' , payload: false })
     }, 1000);    
 
-  } catch (error) {
-    console.log(error)
-    message.error('Email/CPF já existe na base! ')
-    dispatch({ type: 'LOADING', payload: false})
+  } else {
+    message.error(response.data.message)
+    dispatch({ type: 'LOADING' , payload: false })
   }
 }
